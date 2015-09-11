63ab57bf4c078c63baaaa90f7a7ac109470d26d7
Moisés Cervantes <cervantes.moises@me.com>
2015-09-10 19:57:23 -0500

    Scratch

---
+++ Scratchpad.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+//Swift recap
+
+var someVariable = "aVariable"
+
+let someConstant = 20
+
+someVariable = "anotherString"
+
+
+//var fruitsArray: [String] = ["apples"]
+var fruitsArray = ["apples"]
+fruitsArray.append("banana")
+
+fruitsArray
+
+//Optional
+
+var optionalString: String? = "hello"
+
+optionalString!
