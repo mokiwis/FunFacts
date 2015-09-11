57e0d98677bb599d548ed2844cf4932f05e99098
Moisés Cervantes <cervantes.moises@me.com>
2015-09-10 19:35:13 -0500

    Initial Commit

---
+++ Fun Facts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fun Facts
+//
+//  Created by Moisés Cervantes on 10/09/15.
+//  Copyright (c) 2015 Moisés Cervantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
