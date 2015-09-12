6c8db644d675999b55a3ba25a8be6eaaf0c9d7aa
MoiseÃÅs Cervantes <cervantes.moises@me.com>
2015-09-11 19:02:03 -0500

    Conversión a Xcode 7 / Swift 2

--- Fun Facts/FactBook.swift
+++ Fun Facts/FactBook.swift
@@ -61,9 +61,9 @@ struct FactBook {
     
     func randomFact() -> String {
         
-        var unsignedArrayCount = UInt32(factsArray.count)
-        var unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
-        var randomNumber = Int(unsignedRandomNumber)
+        let unsignedArrayCount = UInt32(factsArray.count)
+        let unsignedRandomNumber = arc4random_uniform(unsignedArrayCount)
+        let randomNumber = Int(unsignedRandomNumber)
         
         return factsArray[randomNumber]
     
